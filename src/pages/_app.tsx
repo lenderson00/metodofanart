21029f53e92641380badd8005d9b2a8441eb7e9a
Lenderson Macedo <github@lenderson.com.br>
2022-07-03 12:59:25 -0400

    add pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import router from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('1058744178364766')
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
+  
   return <Component {...pageProps} />
 }
 
