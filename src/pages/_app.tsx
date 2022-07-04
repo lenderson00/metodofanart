a1bec131af3a8d8be2ad76042432df8fba8b0a4a
Lenderson Macedo <github@lenderson.com.br>
2022-07-03 20:32:55 -0400

    iniciar compra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,21 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import TagManager from 'react-gtm-module'
+import { useEffect } from 'react'
  
 const tagManagerArgs = {
     gtmId: 'GTM-PPKCQR6'
 }
+
+
  
-TagManager.initialize(tagManagerArgs)
 
 function MyApp({ Component, pageProps }: AppProps) {
- 
+  useEffect(() => {
+    if (document !== undefined) {
+      TagManager.initialize(tagManagerArgs)
+    }
+  })
   
   return <Component {...pageProps} />
 }
--- src/pages/api/click.ts
+++ src/pages/api/click.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { click } from "../../components/event";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  click()
+    res.send({})
 }
